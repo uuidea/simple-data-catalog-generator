d321ac114db85e914bde3a5f4598b2fb8ab67c5f
joepvgenuchten <joep.van.genuchten@gmail.com>
2026-01-13 16:54:54 +0100

    forgot one

--- src/simple_data_catalog_generator/create_series_page.py
+++ src/simple_data_catalog_generator/create_series_page.py
@@ -1,5 +1,5 @@
 from rdflib import Graph, URIRef, DCAT, RDF
-from page_creation_functions import write_file, get_title, get_description, create_local_link
+from simple_data_catalog_generator.page_creation_functions import write_file, get_title, get_description, create_local_link
 from simple_data_catalog_generator.create_metadata_table import create_metadata_table
 from simple_data_catalog_generator.analysis_functions import was_derived_from_graphic, supply_chain_analysis
 from simple_data_catalog_generator.create_data_quality_table import create_data_quality_table
